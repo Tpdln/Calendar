51ec5efad2e8b8a3a81883da0fa35012d718017d
Tpdln <cbtepedelen@gmail.com>
2026-03-08 19:31:30 +0100

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Cem Berke Tepedelen on 8.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
